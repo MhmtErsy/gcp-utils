08f197fd19fb1d7a42121fdd161b6c80dfb71162
MhmtErsy <mehmet.ersoy1418@gmail.com>
2023-02-19 02:15:57 +0300

    argparse feature was added

--- bigquery_migration.py
+++ bigquery_migration.py
@@ -1,19 +1,34 @@
 import json
 import subprocess
+import argparse
 
-source_project_id = "crdr-lightnift"
-target_project_id = "icat-crdr"
-dataset_id = "crdr"
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--source_project_id")
+parser.add_argument("--source_dataset_id")
+parser.add_argument("--target_project_id")
+parser.add_argument("--target_dataset_id")
+
+
+args = parser.parse_args()
+
+
+source_project_id = args.source_project_id
+target_project_id = args.target_project_id
+source_dataset_id = args.source_dataset_id
+target_dataset_id = args.target_dataset_id
+
+print(source_dataset_id)
 
 tables_json = subprocess.check_output(["bq", "ls", 
                                        "--format=prettyjson",
-                                       f"--project_id={source_project_id}", dataset_id])
+                                       f"--project_id={source_project_id}", source_dataset_id])
 tables = json.loads(tables_json)
 
 for table in tables:
     table_id = table["tableReference"]["tableId"]
-    if table_id != 'cumulative_balance_copy' and table_id != 'contracts':
-        print(f"bq cp {source_project_id}:{dataset_id}.{table_id} {target_project_id}:{dataset_id}.{table_id}")
-        result = subprocess.run(["bq", "cp", f"{source_project_id}:{dataset_id}.{table_id}", 
-                                               f"{target_project_id}:{dataset_id}.{table_id}"],  stdout=subprocess.PIPE)
-        print(result.stdout)
+    print(["bq", "cp", "--force", f"{source_project_id}:{source_dataset_id}.{table_id}", 
+                                         f"{target_project_id}:{target_dataset_id}.{table_id}"])
+    result = subprocess.run(["bq", "cp", "--force", f"{source_project_id}:{source_dataset_id}.{table_id}", 
+                                         f"{target_project_id}:{target_dataset_id}.{table_id}"], stdout=subprocess.PIPE)
+    print(result.stdout)
